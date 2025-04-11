3cce6f4cf4d3163ed369de3cf0aab5c169155887
Durlag <jefferson.brit@gmail.com>
2025-04-11 19:13:31 -0300

    ECS fix

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -261,7 +261,10 @@ data "aws_iam_policy_document" "iam" {
       "iam:AttachRolePolicy",
       "iam:TagRole",
       "iam:TagPolicy",
-      "iam:PassRole"
+      "iam:PassRole",
+      "iam:CreateServiceLinkedRole",
+      "iam:DeleteServiceLinkedRole",
+      "iam:GetServiceLinkedRoleDeletionStatus"
     ]
     resources = ["*"]
   }
